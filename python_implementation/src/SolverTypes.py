b88015bd72d68124d9e801dab45e56b6cb3d03ce
KarolinaMungai <el21kamn@leeds.ac.uk>
2025-11-24 14:34:53 +0000

    Added construct_matrix for Parabolic and Kane solvers

--- python_implementation/src/SolverTypes.py
+++ python_implementation/src/SolverTypes.py
@@ -5,6 +5,7 @@
 # Store classes for different solver types
 from abc import ABC, abstractmethod
 import math
+import numpy as np
 
 import ConstAndScales
 
@@ -29,7 +30,11 @@ class BaseSolver(ABC):
         pass
     
     @abstractmethod
-    def get_coefficient_derivative(self, j, E) :
+    def get_coefficient_derivative(self, j, E):
+        pass
+
+    @abstractmethod
+    def construct_matrix(self):
         pass
 
 
@@ -55,6 +60,23 @@ class ParabolicSolver(BaseSolver):
         dp = self.get_wavevector_derivative(j-1,E)
         dq = self.get_wavevector_derivative(j,E)
         return self.meff(j) / self.meff(j - 1) * (q*dp-p*dq)/(q * q)
+
+    def construct_matrix(self):
+        nz = self.G.get_nz()
+        A = np.zeros(nz)
+        scale = math.pow( (ConstAndScales.HBAR / self.G.get_dz()), 2) / 4.0
+
+        for i in range(1,nz):
+            if i != 1:
+                A[i, i-1] = -scale * (1.0/self.meff(i-1) + 1.0/self.meff[i])
+            if i != nz:
+                A[i, i+1] = -scale * (1.0/self.meff(i+1) + 1.0/self.meff[i])
+            if ( (i != 1) and (i != nz) ):
+                A[i, i] = self.V[i] + scale * (1.0/self.meff(i+1) + 2.0/self.meff(i) + 1.0/self.meff(i-1))
+        
+        A[1, 1] = A[2, 2]
+        A[nz, nz] = A[nz-1, nz-1]
+        return A
 
 class TaylorSolver(BaseSolver):
 
@@ -97,6 +119,61 @@ class KaneSolver(BaseSolver):
         dp = self.get_wavevector_derivative(j-1, E)
         dq = self.get_wavevector_derivative(j, E)
         return self.meff(j) / self.meff(j-1) * (1.0+self.alpha(j)*(E-self.V(j)))/(1.0+self.alpha(j-1)*(E-self.V(j-1)))* (q*dp-p*dq)/(q * q) + p/q * self.meff(j)/self.meff(j-1)*(self.alpha(j) - self.alpha(j-1) + self.alpha(j)*self.alpha(j-1)*(self.V(j) - self.V(j-1))) / (1.0+self.alpha(j-1)*(E-self.V(j-1))) / (1.0+self.alpha(j-1)*(E-self.V(j-1)))
+
+    def construct_matrix(self):
+        nz = self.G.get_nz()
+        A = np.zeros(4*nz)
+        scale = math.pow(ConstAndScales.HBAR / self.G.get_dz(), 2) / 4.0
+        for i in range(1, nz):
+            A_i = 1.0 / self.alpha[i]
+            M_i = A_i / self.meff[i]
+            V_i = self.V[i]
+
+            if (i == 1) or (i == nz):
+                A_plus = 1.0/self.alpha[i]
+                A_minus = A_plus
+                M_plus = A_minus/self.meff[i]
+                M_minus = M_plus
+                V_plus = self.V[i]
+                V_minus = V_plus
+            else:
+                A_minus = 1.0/self.alpha[i-1]
+                A_plus =  1.0/self.alpha[i+1]
+                M_minus = A_minus/self.meff[i-1]
+                M_plus = A_plus/self.meff[i+1]
+                V_minus = self.V[i-1]
+                V_plus = self.V[i+1]
+            
+            B_minus =  A_minus*A_i
+            B_0 = A_minus*A_plus
+            B_plus = A_plus*A_i
+
+            # Add subdiagonals of A0, A1 and A2
+            if i!=1:
+                A[3*nz+i,i-1]       = -scale * (1.0-V_plus/A_plus)*(M_minus*B_plus*(1.0 - V_i/A_i) + M_i*B_0*(1.0-V_minus/A_minus)) 	# A0
+                A[3*nz+i,nz+i-1]    = -scale * (M_i*(A_minus+A_plus-V_minus-V_plus) + M_minus*(A_plus+A_i-V_i-V_plus))				    # A1
+                A[3*nz+i,2*nz+i-1]  = -scale * (M_minus+M_i)																	        # A2
+
+            # Add superdiagonals of A0, A1 and A2
+            if i!=nz:
+                A[3*nz+i,i+1]       = -scale * (1.0-V_minus/A_minus)*(M_plus*B_minus*(1.0 - V_i/A_i) + M_i*B_0*(1.0-V_plus/A_plus))     # A0
+                A[3*nz+i,nz+i+1]    = -scale * (M_i*(A_minus+A_plus-V_minus-V_plus)+M_plus*(A_minus+A_i-V_i-V_minus))				    # A1
+                A[3*nz+i,2*nz+i+1]  = -scale * (M_plus+M_i); 																		    # A2
+
+            # Add diagonal of A0 block
+            A[3*nz+i,i] = -scale * (V_i * (M_plus*A_minus+M_minus*A_plus) + V_minus * (M_plus*A_i+2.0*M_i*A_plus) + V_plus * (M_minus*A_i+2.0*M_i*A_minus) - M_plus*V_i*V_minus - M_minus*V_i*V_plus - 2.0*M_i*V_plus*V_minus - M_plus*B_minus - 2.0*M_i*B_0 - M_minus*B_plus) + V_i * (1.0-V_i/A_i) * (A_i*B_0 - B_plus*V_minus - B_minus*V_plus + A_i*V_minus*V_plus)
+            # Add diagonal of A1 block
+            A[3*nz+i,nz+i] = -scale * (M_plus*(V_i+V_minus-A_i-A_minus) + M_minus*(V_i+V_plus-A_i-A_plus) + 2.0*M_i*(V_minus+V_plus-A_minus-A_plus)) - V_i*V_i*(A_plus+A_minus) + V_i * (B_minus+2.0*B_0+B_plus) + V_minus*B_plus + V_plus*B_minus - V_i*V_minus*(2.0*A_plus+A_i) - V_i*V_plus*(2.0*A_minus+A_i) - A_i*V_minus*V_plus + V_i*V_i*(V_minus+V_plus) + 2.0*V_i*V_minus*V_plus - A_i*B_0
+            # Add diagonal of A2 block
+            A[3*nz+i,2*nz+i] = scale * (M_plus+2.0*M_i+M_minus) - B_plus - B_0 - B_minus + A_plus*(2.0*V_i+V_minus) + A_minus*(2.0*V_i+V_plus) + A_i*(V_i+V_minus+V_plus) - V_i*V_i - 2.0*V_i*(V_minus+V_plus) - V_minus*V_plus
+            # Add diagonal of A3 block
+            A[3*nz+i,3*nz+i] = V_plus+2.0*V_i+V_minus-A_plus-A_i-A_minus
+            
+            # Insert identity matrices
+            A[i,nz+i] = 1.0
+            A[nz+i,2*nz+i] = 1.0
+            A[2*nz+i,3*nz+i] = 1.0           
+        return A
 
 class EkenbergSolver(BaseSolver):
     
