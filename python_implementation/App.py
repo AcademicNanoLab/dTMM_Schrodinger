15e3e4bd1a7d1dc352a0f0fdc9e1bf7a9d5e2025
KarolinaMungai <el21kamn@leeds.ac.uk>
2026-01-29 21:18:50 +0000

    Animation sweep implementation. Needs fixing.

--- python_implementation/App.py
+++ python_implementation/App.py
@@ -12,33 +12,127 @@ from src.Solvers_FDM import Parabolic_FDM, Taylor_FDM, Kane_FDM
 from src.Solvers_TMM import Parabolic_TMM, Taylor_TMM, Kane_TMM, Ekenberg_TMM
 
 import streamlit as st
+import imageio.v2 as imageio
+import numpy as np
 import base64
 import tempfile
 
+class ElectronicStructureApp:
+    def run(self):
+        pg = st.navigation([self.Home, self.ES_Calculator, self.Animation_Sweep])
+        pg.run()
 
-def Home():
-    st.write("# Electronic Structure Calculator")
-    st.write("### Select option using sidebar")
+    def Home(self):
+        st.write("# Electronic Structure Calculator")
+        st.write("### Select option using sidebar")
 
-    col1, col2 = st.columns(2)
-    with col1:
-        st.write("Electronic Structure Calculator")
-        st.image("matlab_implementation/src/optionPng.png")
-    
-    with col2:
-        st.write("Electronic Structure Animation (Bias Sweep)")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("Electronic Structure Calculator")
+            st.image("matlab_implementation/src/optionPng.png")
+        
+        with col2:
+            st.write("Electronic Structure Animation (Bias Sweep)")
 
-        file_ = open("matlab_implementation/src/optionGif.gif", "rb")
-        contents = file_.read()
-        data_url = base64.b64encode(contents).decode("utf-8")
-        file_.close()
+            file_ = open("matlab_implementation/src/optionGif.gif", "rb")
+            contents = file_.read()
+            data_url = base64.b64encode(contents).decode("utf-8")
+            file_.close()
 
-        st.markdown(
-            f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
-            unsafe_allow_html=True,
-        )    
+            st.markdown(
+                f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+                unsafe_allow_html=True,
+            )    
 
-def set_options(sweep=False):
+    def ES_Calculator(self):
+        st.title("Electronic Structure Calculator")
+
+        file, material, nstmax, solver, nonparabolicityType, dz, padding= set_options()
+        K = st.number_input("K (kV/cm)", 0.0, 5.0, step=0.1, value = 1.9)
+
+        solve = st.button("Calculate") 
+
+        if solve:
+            G = Grid(file, dz, material)
+            G.set_K(K)
+
+            Solver = SolverFactory.create(G, solver, nonparabolicityType, nstmax)
+
+            [energies, psis] = Solver.get_wavefunctions()
+            energies_meV = energies / src.ConstAndScales.E
+            V = Visualisation(G, energies, psis)
+            
+            st.plotly_chart(V.plot_V_wf())
+            st.plotly_chart(V.plot_energies())
+            st.plotly_chart(V.plot_energy_diff_thz())
+            st.plotly_chart(V.plot_QCL(K, padding, False, None))
+
+    def Animation_Sweep(self):
+        st.title("Electronic Structure Animation (Bias Sweep)")
+
+        file, material, nstmax, solver, nonparabolicityType, dz, padding = set_options()
+
+        c1, c2, c3 = st.columns(3)
+        with c1:
+            kmin = st.number_input("Kmin (kV/cm)", 0.0, 5.0, step=0.1, value =1.0)
+        with c2:
+            kmax = st.number_input("Kmax (kV/cm)", 0.0, 5.0, step=0.1, value =2.0)
+        with c3:
+            kstep = st.number_input("Step (kV/cm)", 0.0, 5.0, step=0.1, value = 0.5)
+
+        st.write("Set Axis Limits")
+        col1, col2, = st.columns(2)
+        with col1:
+            xmin = st.number_input("Xmin: ", 0, 3000, step=100, value=0)
+            xmax = st.number_input("Xmax: ", 0, 3000, step=100, value=2000)
+                
+        with col2:
+            ymin = st.number_input("Ymin: ", 0, 200, step=10, value=0)
+            ymax = st.number_input("Ymax: ", 0, 200, step=10, value=120)
+
+        solve = st.button("Calculate") 
+        if solve:
+            frames = []
+
+            G = Grid(file, dz, material)
+            k_values: list[float] = np.arange(kmin, kmax, kstep).tolist()
+            for k in k_values:
+                G.set_K(k)
+
+                Solver = SolverFactory().create(G, solver, nonparabolicityType, nstmax)
+                [energies, psis] = Solver.get_wavefunctions()
+                V = Visualisation(G, energies, psis)
+                axisLimits = [xmin, xmax, ymin, ymax]
+                fig = V.plot_QCL(k, padding, True, axisLimits)
+
+                with tempfile.NamedTemporaryFile(suffix=".png") as tmp:
+                    fig.write_image(tmp.name)
+                    frames.append(imageio.imread(tmp.name))
+            
+            # write gif
+            gif_path = tempfile.NamedTemporaryFile(delete=False, suffix=".gif").name
+            imageio.mimsave(gif_path, frames, fps=4)
+
+            # show in streamlit
+            st.image(gif_path)
+
+
+class SolverFactory:
+    solver_map = {
+        ("FDM", "Parabolic"): Parabolic_FDM,
+        ("FDM", "Taylor"): Taylor_FDM,
+        ("FDM", "Kane"): Kane_FDM,
+        ("TMM", "Parabolic"): Parabolic_TMM,
+        ("TMM", "Taylor"): Taylor_TMM,
+        ("TMM", "Kane"): Kane_TMM,
+        ("TMM", "Ekenberg"): Ekenberg_TMM,
+    }
+
+    @staticmethod
+    def create(grid, solver, np_type, nstmax):
+        return SolverFactory.solver_map[(solver, np_type)](grid, nstmax)
+
+def set_options():
     st.markdown("### Select your options")
 
     file = st.file_uploader("Pick a file", type="TXT")
@@ -55,82 +149,16 @@ def set_options(sweep=False):
     np_options = ["Parabolic", "Taylor", "Kane", "Ekenberg"] if solver == "TMM" else ["Parabolic", "Taylor", "Kane"]
     np_type = st.radio("Non-parabolicity type", np_options, horizontal=True)
 
-    c1, c2, c3, c4 = st.columns(4)
+    c1, c2, c3 = st.columns(3)
 
     with c1:
-        k = st.number_input("K (kV/cm)", 0.0, 5.0, step=0.1, value = 1.9)
-
-    with c2:
         nstmax = st.number_input("Nst max", 0, 20, value=10)
-    with c3:
+    with c2:
         dz = st.number_input("dz (Å)", 0, 2, value=1)
-    with c4:
+    with c3:
         pad = st.number_input("Padding (Å)", 0, 500)
     
-    return tmp_path, material, k, nstmax, solver, np_type, dz, pad
+    return tmp_path, material, nstmax, solver, np_type, dz, pad
 
-
-def ES_Calculator():
-    st.title("Electronic Structure Calculator")
-
-    file, material, K, nstmax, solver, nonparabolicityType, dz, padding= set_options()
-
-    solve = st.button("Calculate") 
-
-    if solve:
-        G = Grid(file, dz, material)
-        G.set_K(K)
-
-        if solver == "FDM":
-            if nonparabolicityType == "Parabolic":
-                Solver = Parabolic_FDM(G, nstmax)
-            elif nonparabolicityType == "Taylor":
-                Solver = Taylor_FDM(G, nstmax)
-            elif nonparabolicityType == "Kane":
-                Solver = Kane_FDM(G, nstmax)
-
-        elif solver == "TMM":
-            if nonparabolicityType == "Parabolic":
-                Solver = Parabolic_TMM(G, nstmax)
-            elif nonparabolicityType == "Taylor":
-                Solver = Taylor_TMM(G, nstmax)
-            elif nonparabolicityType == "Kane":
-                Solver = Kane_TMM(G, nstmax)
-            elif nonparabolicityType == "Ekenberg":
-                Solver = Ekenberg_TMM(G, nstmax)
-
-        [energies, psis] = Solver.get_wavefunctions()
-        energies_meV = energies / src.ConstAndScales.E
-        V = Visualisation(G, energies, psis)
-        
-        st.plotly_chart(V.plot_V_wf())
-        st.plotly_chart(V.plot_energies())
-        st.plotly_chart(V.plot_energy_diff_thz())
-        st.plotly_chart(V.plot_QCL(K, padding, False, None))
-
-def Animation_Sweep():
-    st.title("Electronic Structure Animation (Bias Sweep)")
-
-    file, material, K, nstmax, solver, nonparabolicityType, dz, padding = set_options(sweep = True)
-
-    kmin = st.number_input("Kmin (kV/cm)", 0.0, 5.0, step=0.1, value =1.0)
-    kmax = st.number_input("Kmax (kV/cm)", 0.0, 5.0, step=0.1, value =2.0)
-    kstep = st.number_input("Step (kV/cm)", 0.0, 5.0, step=0.1, value = 0.5)
-
-    k_sweep = kmin, kmax, kstep
-
-
-    st.write("Set Axis Limits")
-    col1, col2, = st.columns(2)
-    with col1:
-        xmin = st.number_input("Xmin: ", 0, 3000, step=100)
-        xmax = st.number_input("Xmax: ", 0, 3000, step=100)
-            
-    with col2:
-        ymin = st.number_input("Ymin: ", 0, 200, step=10)
-        ymax = st.number_input("Xmax: ", 0, 200, step=10)
-
-    st.write("TODO: Complete Animation Sweep function.")
-
-pg = st.navigation([Home, ES_Calculator, Animation_Sweep])
-pg.run()
+app = ElectronicStructureApp()
+app.run()
