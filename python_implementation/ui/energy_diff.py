94620d5b0e720e8a16f978726916b5e7c28d56a7
KarolinaMungai <el21kamn@leeds.ac.uk>
2026-04-09 15:44:46 +0100

    Reformat: energy_diff plots done using sweep_visualisation

--- python_implementation/ui/energy_diff.py
+++ python_implementation/ui/energy_diff.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+import src.Composition
+
 class EnergyDifferencePage:
     def render(self):
         st.title("Energy Difference Plots")
@@ -14,9 +16,8 @@ class EnergyDifferencePage:
         Inputs.render_energy_diff_inputs()
 
         ### Calculate
-        import plotly.graph_objects as go
+        import src.ConstAndScales
         from src.TransitionCalculator import TransitionCalculator
-        fig = go.Figure()
 
         if None in (Inputs.solver, Inputs.composition, Inputs.sweep_param, Inputs.plot_diff):
             st.markdown(":red-badge[**<Calculate> button only appears once all fields are filled.**]")
@@ -25,18 +26,20 @@ class EnergyDifferencePage:
                 progress_text = "Calculating. Please wait."
                 pbar_val = 0
                 p_bar = st.progress(pbar_val, text=progress_text)
-                fig = go.Figure()
-                height_fig = go.Figure()
-                h_trace = []
+
+                ediff_trace = []
+                dipole_trace = []
+                osc_str_trace = []
+
+                plot_heights = []
                 for h in Inputs.heights:
-                    x_width = []
-                    trace = []
+                    plot_widths = []
                     for w in Inputs.widths:
                         arr = Inputs.composition.as_array() # type: ignore
                         arr[1][0] = w
                         arr[0][1] = h
                         arr[-1][1] = h
-
+                    
                         C2 = Composition.from_array(arr)
                         
                         # setup for calculation
@@ -51,50 +54,39 @@ class EnergyDifferencePage:
                         j = 1
 
                         T = TransitionCalculator()
-
                         ediff, dipoles, osc_str = T.calculate(G.z, energies, wavefunctions, i, j)
+
                         if None not in (ediff, dipoles, osc_str):
-                            x_width.append(w)
+                            plot_widths.append(w)
+                            plot_heights.append(h)
 
-
-                        if Inputs.plot_diff == "Energy Difference":
-                            trace.append(T.get_energy_diff(energies, i,j))
-                        elif Inputs.plot_diff == "Dipole matrix element":
-                            tmp = T.get_dipole(G.z, wavefunctions, i,j)
-                            if tmp is not None:
-                                trace.append(tmp /1e-9)
-                        elif Inputs.plot_diff == "Oscillator Strength":
-                            trace.append(T.get_oscillator_strength(G.z, energies, wavefunctions, i,j))
-                        x_width.append(w)
-
+                            ediff_trace.append(ediff / src.ConstAndScales.meV) # type: ignore
+                            dipole_trace.append(dipoles / src.ConstAndScales.nano) # type: ignore
+                            osc_str_trace.append(osc_str / src.ConstAndScales.E) # type: ignore
+                        
                         pbar_val += int(100/(len(Inputs.heights)*len(Inputs.widths)))
                         p_bar.progress(pbar_val, progress_text)
-                        
-                    if len(energies) > 1:
-                        h_trace.append(energies[1] - energies[0])
 
-                    fig.add_trace(go.Scatter(
-                        x=x_width,
-                        y=trace,
-                        mode='lines+markers',
-                        name=f"h = {h:.2f}"
-                    ))
+                from .sweep_visualisation import SweepVisualisation
 
-                height_fig.add_trace(go.Scatter(
-                    x = Inputs.heights,
-                    y = h_trace,
-                    mode= 'lines+markers',
-                    name = f"w = {w}"
-                ))
+                match Inputs.sweep_param:
+                    case "Sweep Well Width":
+                        Energies = SweepVisualisation(ediff_trace, plot_widths, "Width (Å)", "Energy (meV)", f"(E{i}-E{j}) vs Quantum Well Width")
+                        Dipoles = SweepVisualisation(dipole_trace, plot_widths, "Width (Å)", "Dipole Moment", f"(D{i}-D{j}) vs Quantum Well Width")
+                        Oscillator_Str = SweepVisualisation(osc_str_trace, plot_widths, "Width (Å)", "Oscillator Strength", f"(O{i}-O{j}) vs Quantum Well Width")
+
+                        st.plotly_chart(Energies.single_sweep_plot())
+                        st.plotly_chart(Dipoles.single_sweep_plot())
+                        st.plotly_chart(Oscillator_Str.single_sweep_plot())
+                    
+                    case "Sweep Barrier Height":
+                        Energies = SweepVisualisation(ediff_trace, plot_heights, "Height", "Energy (meV)", f"(E{i}-E{j}) vs Quantum Well Width")
+                        Dipoles = SweepVisualisation(dipole_trace, plot_heights, "Height", "Dipole Moment", f"(D{i}-D{j}) vs Quantum Well Width")
+                        Oscillator_Str = SweepVisualisation(osc_str_trace, plot_heights, "Height", "Oscillator Strength", f"(O{i}-O{j}) vs Quantum Well Width")
+
+                        st.plotly_chart(Energies.single_sweep_plot())
+                        st.plotly_chart(Dipoles.single_sweep_plot())
+                        st.plotly_chart(Oscillator_Str.single_sweep_plot())
 
                 p_bar.empty()
 
-                if Inputs.sweep_param == "Sweep Barrier Height":
-                    st.plotly_chart(height_fig)
-                else:
-                    fig.update_layout(
-                        title="(E2 - E1) vs. quantum well width",
-                        xaxis_title="Width (Å)",
-                        yaxis_title="Energy difference (meV)"
-                    )
-                    st.plotly_chart(fig)
