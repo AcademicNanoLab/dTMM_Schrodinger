Off by one errors in FDM fixed

python_implementation/src/Solvers_FDM.py
@@ -2,7 +2,6 @@
 #
 # Store classes for different FDM Solvers
 
-import scipy.sparse
 from src.FDMSolver import FDMSolver
 from src import ConstAndScales
 
@@ -22,11 +21,11 @@ class Parabolic_FDM(FDMSolver): # type: ignore
         scale = math.pow( (ConstAndScales.HBAR / self.G.get_dz()), 2) / 4.0
 
         for i in range(nz-1):
-            if i != 1:
+            if i != 0:
                 A[i, i-1] = -scale * (1.0/self.meff[i-1] + 1.0/self.meff[i])
-            if i != nz:
+            if i != nz-1:
                 A[i, i+1] = -scale * (1.0/self.meff[i+1] + 1.0/self.meff[i])
-            if ( (i != 1) and (i != nz) ):
+            if ( (i != 0) and (i != nz-1) ):
                 A[i, i] = self.V[i] + scale * (1.0/self.meff[i+1] + 2.0/self.meff[i] + 1.0/self.meff[i-1])
         
         A[0, 0] = A[1, 1]
@@ -50,7 +49,7 @@ class Kane_FDM(FDMSolver):      # type: ignore
             V_i = self.V[i]
 
             # Handle boundaries
-            if (i == 1) or (i == nz-1):
+            if (i == 0) or (i == nz-1):
                 A_plus = 1.0/self.alpha[i]
                 A_minus = A_plus
                 M_plus = A_minus/self.meff[i]
@@ -70,7 +69,7 @@ class Kane_FDM(FDMSolver):      # type: ignore
             B_plus = A_plus*A_i
 
             # Add subdiagonals of A0, A1 and A2
-            if i!=1:
+            if i!=0:
                 A[3*nz+i,i-1]       = -scale * (1.0-V_plus/A_plus)*(M_minus*B_plus*(1.0 - V_i/A_i) + M_i*B_0*(1.0-V_minus/A_minus)) 	# A0
                 A[3*nz+i,nz+i-1]    = -scale * (M_i*(A_minus+A_plus-V_minus-V_plus) + M_minus*(A_plus+A_i-V_i-V_plus))				    # A1
                 A[3*nz+i,2*nz+i-1]  = -scale * (M_minus+M_i)																	        # A2
@@ -106,17 +105,18 @@ class Taylor_FDM(FDMSolver):    # type: ignore
         nz = self.G.get_nz()
         # A = np.zeros((nz, nz))
         A = sp.lil_matrix((nz, nz))
-        B = A
+        B = sp.lil_matrix((nz, nz))
+
         scale = math.pow(ConstAndScales.HBAR/self.G.get_dz(), 2) / 4.0
 
         for i in range(nz):
-            if i != 1:
+            if i != 0:
                 B[i, i-1] = -scale * (self.alpha[i] / self.meff[i] + self.alpha[i-1] / self.meff[i-1])
                 A[i, i-1] = -scale * ((1.0+self.alpha[i-1] *self.V[i-1]) / self.meff[i-1] + (1.0+self.alpha[i]*self.V[i])/self.meff[i])
             if i != nz-1:
                 B[i, i+1] = -scale * (self.alpha[i] / self.meff[i] + self.alpha[i+1] / self.meff[i+1])
                 A[i, i+1] = -scale * ((1.0+self.alpha[i+1]*self.V[i+1])/self.meff[i+1]+(1.0+self.alpha[i]*self.V[i])/self.meff[i])
-            if (i!=1) and (i!=nz-1):
+            if (i!=0) and (i!=nz-1):
                 B[i,i] = 1.0 + scale * (self.alpha[i+1] / self.meff[i+1] + 2.0 * self.alpha[i] / self.meff[i] + self.alpha[i-1] / self.meff[i-1])
                 A[i,i] = self.V[i] + scale * ((1.0+self.alpha[i+1]*self.V[i+1])/self.meff[i+1] + 2.0 * (1.0+self.alpha[i]*self.V[i])/self.meff[i] + (1.0+self.alpha[i-1]*self.V[i-1])/self.meff[i-1])
 			    